61fab07aad62830e050571b1cf44dcbc55202d94
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-24 12:15:53 -0600

    initial get routes

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//  routes 
+app.get("/", function (req, res){
+    //  send back index html page 
+    res.sendFile(path.join(__dirname, "index.html"));
+    
+});
+
+app.get("/view", function(req, res){
+    //send view.html
+    res.sendFile(path.join(__dirname, "view.html"));
+    
+})
+
 //  start server 
 
 app.listen(PORT, function() {
